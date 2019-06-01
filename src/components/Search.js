5571c36c05c9fae088f7a59243ff1d5447919199
Frank Rosendorf <frank.rosendorf1@gmail.com>
2019-06-01 18:12:26 -0400

    Refactor Search component to use material UI

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,54 +1,93 @@
 import React, { Component } from 'react';
-import {Route} from 'react-router-dom';
-import Aux from '../hoc/Auxiliary';
+import { Route } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import Button from '@material-ui/core/Button';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
 import Images from './Images';
 
 class Search extends Component {
-
   state = {
     search: ''
-  }
+  };
 
   handleChange = e => {
-    const {value} = e.target;
+    const { value } = e.target;
     this.setState({ search: value });
-  }
+  };
 
   handleSubmit = e => {
     e.preventDefault();
     const newPath = `/search/${this.state.search}`;
     this.props.history.push(newPath);
-    this.setState({search: ''});
-  }
+    this.setState({ search: '' });
+  };
 
   render() {
-    const {search} = this.state;
-    const {match: {path}} = this.props
+    const { search } = this.state;
+    const {
+      match: { path },
+      classes
+    } = this.props;
     return (
-      <Aux>
-        <form 
-          className="search-form" 
-          action="post"
-          onSubmit={this.handleSubmit}>
-            <input 
-              type="search"
-              name="search"
-              placeholder="Search Images!"
-              onChange={this.handleChange}
-              value={search}
-              required/>
-            <button
-              type="submit">
-                <svg fill="#fff" height="24" viewBox="0 0 23 23" width="24" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
-                  <path d="M0 0h24v24H0z" fill="none"/>
-                </svg>
-            </button>
+      <>
+        <form className={classes.root} onSubmit={this.handleSubmit}>
+          <Button type='submit' className={classes.searchIcon}>
+            <SearchIcon />
+          </Button>
+          <InputBase
+            placeholder='Search images!'
+            type='search'
+            required
+            autoFocus={true}
+            onChange={this.handleChange}
+            value={search}
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput
+            }}
+          />
         </form>
-        <Route path={`${path}/:query`} render={ ({match}) => <Images searchQuery={match.params.query} /> } />
-      </Aux>
+        <Route
+          path={`${path}/:query`}
+          render={({ match }) => <Images searchQuery={match.params.query} />}
+        />
+      </>
     );
   }
 }
 
-export default Search;
+const styles = theme => ({
+  root: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    margin: '0 auto',
+    width: '60%',
+    backgroundColor: fade(theme.palette.primary.dark, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.primary.dark, 0.25)
+    }
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    cursor: 'pointer',
+    backgroundColor: theme.palette.primary.light
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
+    paddingLeft: '15px'
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width')
+  }
+});
+
+export default withStyles(styles)(Search);
